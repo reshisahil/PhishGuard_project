c486229faf318107436942716cabe0a09fe1f8cf
HEMANTH KUMAR R <hemanthhemanth70580@gmail.com>
2026-08-29 16:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -704,7 +704,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const response =
                     await fetch(
-                        'http://127.0.0.1:5000/predict',
+                        '/predict',
                         {
 
                             method: 'POST',
